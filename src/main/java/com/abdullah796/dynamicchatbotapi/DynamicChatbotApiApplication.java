453749c71dd90e4b970f31964420042878ed4b37
Abdullah <git@plumptech.com>
2022-06-28 18:48:32 +0500

    Project added

---
+++ src/main/java/com/abdullah796/dynamicchatbotapi/DynamicChatbotApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdullah796.dynamicchatbotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicChatbotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicChatbotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdullah796/dynamicchatbotapi/DynamicChatbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah796.dynamicchatbotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicChatbotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
